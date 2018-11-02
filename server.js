5412e0e624c059049e69603bcdc997188e5560d5
Cédric Bazureau <cbazureau@gmail.com>
2018-11-02 13:17:07 +0100

    feat(init): prettier

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const {
-  estimatePlay,
-  getOppositeColor,
-  STATUS,
-  COLOR_LIST,
-  DEFAULT_COLOR,
-  MATRIX_WIDTH,
-  MATRIX_HEIGHT,
+	estimatePlay,
+	getOppositeColor,
+	STATUS,
+	COLOR_LIST,
+	DEFAULT_COLOR,
+	MATRIX_WIDTH,
+	MATRIX_HEIGHT,
 } = require('./utils/rules');
 
 let matrix = [];
@@ -19,19 +19,19 @@ let NEXT_PLAYER = COLOR_LIST[0];
  * initGame
  */
 const initGame = () => {
-  matrix = [];
-  for (var i = 0; i < MATRIX_HEIGHT; i++) {
-    if (!matrix[i]) matrix[i] = [];
-    for (var j = 0; j < MATRIX_WIDTH; j++) {
-      matrix[i][j] = DEFAULT_COLOR;
-    }
-  }
-  matrix[3][3] = COLOR_LIST[0];
-  matrix[3][4] = COLOR_LIST[1];
-  matrix[4][4] = COLOR_LIST[0];
-  matrix[4][3] = COLOR_LIST[1];
-  STARTED = true;
-  NEXT_PLAYER = COLOR_LIST[0];
+	matrix = [];
+	for (var i = 0; i < MATRIX_HEIGHT; i++) {
+		if (!matrix[i]) matrix[i] = [];
+		for (var j = 0; j < MATRIX_WIDTH; j++) {
+			matrix[i][j] = DEFAULT_COLOR;
+		}
+	}
+	matrix[3][3] = COLOR_LIST[0];
+	matrix[3][4] = COLOR_LIST[1];
+	matrix[4][4] = COLOR_LIST[0];
+	matrix[4][3] = COLOR_LIST[1];
+	STARTED = true;
+	NEXT_PLAYER = COLOR_LIST[0];
 };
 
 /**
@@ -40,12 +40,12 @@ const initGame = () => {
  * @param {*} res
  */
 const emitRefresh = (req, res, errorName) => {
-  const uri = req ? req.url : '';
-  const status = errorName ? errorName : 'OK';
-  const next = NEXT_PLAYER;
-  const result = { matrix, uri, status, next };
-  io.sockets.emit('refresh', result);
-  if (res && req) res.send(result);
+	const uri = req ? req.url : '';
+	const status = errorName ? errorName : 'OK';
+	const next = NEXT_PLAYER;
+	const result = { matrix, uri, status, next };
+	io.sockets.emit('refresh', result);
+	if (res && req) res.send(result);
 };
 
 /**
@@ -57,36 +57,36 @@ const emitRefresh = (req, res, errorName) => {
  * @param {*} color
  */
 const play = ({ req, res, i, j, color }) => {
-  if (!STARTED) {
-    emitRefresh(req, res, STATUS.NOT_STARTED);
-    return;
-  }
+	if (!STARTED) {
+		emitRefresh(req, res, STATUS.NOT_STARTED);
+		return;
+	}
 
-  if (i === undefined || j === undefined || color === undefined) {
-    emitRefresh(req, res, STATUS.MISSING_PARAMS);
-    return;
-  }
+	if (i === undefined || j === undefined || color === undefined) {
+		emitRefresh(req, res, STATUS.MISSING_PARAMS);
+		return;
+	}
 
-  const iInt = parseInt(i, 10);
-  const jInt = parseInt(j, 10);
+	const iInt = parseInt(i, 10);
+	const jInt = parseInt(j, 10);
 
-  if (NEXT_PLAYER !== color) {
-    emitRefresh(req, res, STATUS.NOT_YOUR_TURN);
-    return;
-  }
+	if (NEXT_PLAYER !== color) {
+		emitRefresh(req, res, STATUS.NOT_YOUR_TURN);
+		return;
+	}
 
-  const estimation = estimatePlay(matrix, iInt, jInt, color);
+	const estimation = estimatePlay(matrix, iInt, jInt, color);
 
-  if (estimation.status !== STATUS.OK) {
-    emitRefresh(req, res, estimation.status);
-    return;
-  }
+	if (estimation.status !== STATUS.OK) {
+		emitRefresh(req, res, estimation.status);
+		return;
+	}
 
-  matrix[iInt][jInt] = color;
-  estimation.results.forEach(p => (matrix[p[0]][p[1]] = color));
-  NEXT_PLAYER = getOppositeColor(color);
+	matrix[iInt][jInt] = color;
+	estimation.results.forEach((p) => (matrix[p[0]][p[1]] = color));
+	NEXT_PLAYER = getOppositeColor(color);
 
-  emitRefresh(req, res, STATUS.OK);
+	emitRefresh(req, res, STATUS.OK);
 };
 
 // our localhost port
@@ -95,20 +95,20 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/status', (req, res) => {
-  res.send({
-    status: STATUS.OK,
-    matrix,
-  });
+	res.send({
+		status: STATUS.OK,
+		matrix,
+	});
 });
 
 app.get('/restart', (req, res) => {
-  initGame();
-  emitRefresh(req, res);
+	initGame();
+	emitRefresh(req, res);
 });
 
 app.get('/play/:color/:i/:j', (req, res) => {
-  const { i, j, color } = req.params;
-  play({ req, res, i, j, color });
+	const { i, j, color } = req.params;
+	play({ req, res, i, j, color });
 });
 
 // our server instance
@@ -119,10 +119,10 @@ initGame();
 
 // This creates our socket using the instance of the server
 const io = socketIO(server);
-io.on('connection', socket => {
-  console.log('[socket] user connected');
-  emitRefresh();
-  socket.on('play', ({ i, j, color }) => play({ i, j, color }));
-  socket.on('disconnect', () => console.log('[socket] user disconnected'));
+io.on('connection', (socket) => {
+	console.log('[socket] user connected');
+	emitRefresh();
+	socket.on('play', ({ i, j, color }) => play({ i, j, color }));
+	socket.on('disconnect', () => console.log('[socket] user disconnected'));
 });
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- utils/rules.js
+++ utils/rules.js
@@ -1,24 +1,24 @@
 const DEFAULT_COLOR = 'green';
-const COLOR_LIST = ['white', 'black'];
+const COLOR_LIST = [ 'white', 'black' ];
 const MATRIX_WIDTH = 8;
 const MATRIX_HEIGHT = 8;
 
 const STATUS = {
-  OK: 'OK',
-  ANOTHER_PLAYER_HERE: 'ANOTHER_PLAYER_HERE',
-  IMPOSSIBLE_PLAY: 'IMPOSSIBLE_PLAY',
-  NOT_YOUR_TURN: 'NOT_YOUR_TURN',
-  NOT_STARTED: 'NOT_STARTED',
-  MISSING_PARAMS: 'MISSING_PARAMS',
+	OK: 'OK',
+	ANOTHER_PLAYER_HERE: 'ANOTHER_PLAYER_HERE',
+	IMPOSSIBLE_PLAY: 'IMPOSSIBLE_PLAY',
+	NOT_YOUR_TURN: 'NOT_YOUR_TURN',
+	NOT_STARTED: 'NOT_STARTED',
+	MISSING_PARAMS: 'MISSING_PARAMS',
 };
 
 /**
  * getOppositeColor
  * @param {*} color
  */
-const getOppositeColor = color => {
-  const nextIndex = (COLOR_LIST.indexOf(color) + 1) % COLOR_LIST.length;
-  return COLOR_LIST[nextIndex];
+const getOppositeColor = (color) => {
+	const nextIndex = (COLOR_LIST.indexOf(color) + 1) % COLOR_LIST.length;
+	return COLOR_LIST[nextIndex];
 };
 
 /**
@@ -31,32 +31,32 @@ const getOppositeColor = color => {
  * @param {*} color
  */
 const checkDirection = (matrix, iTest, jTest, iVector, jVector, color) => {
-  const flippableCases = [];
-  const iStart = iTest + iVector;
-  const jStart = jTest + jVector;
-  let i = iStart;
-  let j = jStart;
-  if (
-    !matrix[iStart] ||
-    !matrix[iStart][jStart] ||
-    matrix[iStart][jStart] === DEFAULT_COLOR ||
-    matrix[iStart][jStart] === color
-  ) {
-    return [];
-  }
-  flippableCases.push([iStart, jStart]);
-  while (i < MATRIX_HEIGHT && i > 0 && j < MATRIX_WIDTH && j > 0) {
-    if (matrix[i][j] === DEFAULT_COLOR) {
-      return [];
-    }
-    if (matrix[i][j] === color) {
-      return flippableCases;
-    }
-    flippableCases.push([i, j]);
-    i = i + iVector;
-    j = j + jVector;
-  }
-  return [];
+	const flippableCases = [];
+	const iStart = iTest + iVector;
+	const jStart = jTest + jVector;
+	let i = iStart;
+	let j = jStart;
+	if (
+		!matrix[iStart] ||
+		!matrix[iStart][jStart] ||
+		matrix[iStart][jStart] === DEFAULT_COLOR ||
+		matrix[iStart][jStart] === color
+	) {
+		return [];
+	}
+	flippableCases.push([ iStart, jStart ]);
+	while (i < MATRIX_HEIGHT && i > 0 && j < MATRIX_WIDTH && j > 0) {
+		if (matrix[i][j] === DEFAULT_COLOR) {
+			return [];
+		}
+		if (matrix[i][j] === color) {
+			return flippableCases;
+		}
+		flippableCases.push([ i, j ]);
+		i = i + iVector;
+		j = j + jVector;
+	}
+	return [];
 };
 
 /**
@@ -67,42 +67,42 @@ const checkDirection = (matrix, iTest, jTest, iVector, jVector, color) => {
  * @param {*} color
  */
 const estimatePlay = (matrix, iInt, jInt, color) => {
-  if (matrix[iInt][jInt] !== DEFAULT_COLOR) {
-    return {
-      status: STATUS.ANOTHER_PLAYER_HERE,
-    };
-  }
-  const results = [
-    ...checkDirection(matrix, iInt, jInt, -1, -1, color),
-    ...checkDirection(matrix, iInt, jInt, -1, 0, color),
-    ...checkDirection(matrix, iInt, jInt, -1, 1, color),
-    ...checkDirection(matrix, iInt, jInt, 0, -1, color),
-    ...checkDirection(matrix, iInt, jInt, 0, 0, color),
-    ...checkDirection(matrix, iInt, jInt, 0, 1, color),
-    ...checkDirection(matrix, iInt, jInt, 1, -1, color),
-    ...checkDirection(matrix, iInt, jInt, 1, 0, color),
-    ...checkDirection(matrix, iInt, jInt, 1, 1, color),
-  ];
+	if (matrix[iInt][jInt] !== DEFAULT_COLOR) {
+		return {
+			status: STATUS.ANOTHER_PLAYER_HERE,
+		};
+	}
+	const results = [
+		...checkDirection(matrix, iInt, jInt, -1, -1, color),
+		...checkDirection(matrix, iInt, jInt, -1, 0, color),
+		...checkDirection(matrix, iInt, jInt, -1, 1, color),
+		...checkDirection(matrix, iInt, jInt, 0, -1, color),
+		...checkDirection(matrix, iInt, jInt, 0, 0, color),
+		...checkDirection(matrix, iInt, jInt, 0, 1, color),
+		...checkDirection(matrix, iInt, jInt, 1, -1, color),
+		...checkDirection(matrix, iInt, jInt, 1, 0, color),
+		...checkDirection(matrix, iInt, jInt, 1, 1, color),
+	];
 
-  if (results.length === 0) {
-    return {
-      status: STATUS.IMPOSSIBLE_PLAY,
-    };
-  }
+	if (results.length === 0) {
+		return {
+			status: STATUS.IMPOSSIBLE_PLAY,
+		};
+	}
 
-  return {
-    status: STATUS.OK,
-    results,
-  };
+	return {
+		status: STATUS.OK,
+		results,
+	};
 };
 
 module.exports = {
-  DEFAULT_COLOR,
-  COLOR_LIST,
-  STATUS,
-  MATRIX_WIDTH,
-  MATRIX_HEIGHT,
-  checkDirection,
-  getOppositeColor,
-  estimatePlay,
+	DEFAULT_COLOR,
+	COLOR_LIST,
+	STATUS,
+	MATRIX_WIDTH,
+	MATRIX_HEIGHT,
+	checkDirection,
+	getOppositeColor,
+	estimatePlay,
 };
